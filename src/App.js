dd9a96c76cbfc069eb0eaf9c92f1af89dab4d490
Arash Zarei <arashzarei.js@gmail.com>
2023-03-30 17:06:44 +0430

    add title page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="App">
-      
+      <h1 className="h1 text-center font-primary mt-2">جدول نتاوبی</h1>
     </div>
   );
 }
