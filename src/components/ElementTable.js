0cafa8d36acdecf48276ef24264362ea34f68416
Arash Zarei <arashzarei.js@gmail.com>
2023-03-31 18:08:56 +0430

    add react router dom in project and add link for element table.

--- src/components/ElementTable.js
+++ src/components/ElementTable.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
+
+// RactRouterDom
+import { Link } from "react-router-dom";
 
 const ElementTable = ({ data }) => {
-    const { symbol, name, atomicMass, atomicNum, category } = data
-    return (
-        <div className={`w-20 h-24 flex flex-col cursor-pointer m-[1px] hover:border-black hover:border-[2px] ${category}`}>
-            <p className='font-table text-[12px] pl-2'>{atomicNum}</p>
-            <div className='flex flex-col items-center'>
-                <p className='font-table text-[18px]'>{symbol}</p>
-                <p className='text-lg font-primary text-[13px] font-bold'>{name}</p>
-                <p className='text-[12px] font-table'>{atomicMass}</p>
-            </div>
+  const { symbol, name, atomicMass, atomicNum, category } = data;
+  return (
+    <Link to={symbol}>
+      <div
+        className={`w-20 h-24 flex flex-col cursor-pointer m-[1px] hover:border-black hover:border-[2px] ${category}`}
+      >
+        <p className="font-table text-[12px] pl-2">{atomicNum}</p>
+        <div className="flex flex-col items-center">
+          <p className="font-table text-[18px]">{symbol}</p>
+          <p className="text-lg font-primary text-[13px] font-bold">{name}</p>
+          <p className="text-[12px] font-table">{atomicMass}</p>
         </div>
-    );
+      </div>
+    </Link>
+  );
 };
 
 export default ElementTable;
