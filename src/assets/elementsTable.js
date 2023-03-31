817cdf930308dfa0424c4f057a6b9f3dc7aab860
Arash Zarei <arashzarei.js@gmail.com>
2023-03-31 13:19:45 +0430

    create elements table data.

---
+++ src/assets/elementsTable.js
@@ -0,0 +1,770 @@
+const period_1 =[
+    {
+        symbol: "H",
+        name: "هیدروژن",
+        atomicMass: 1.008,
+        atomicNum: 1,
+    },
+    {
+        symbol: "He",
+        name: "هلیم",
+        atomicMass: 4.003,
+        atomicNum: 2,
+    }
+]
+
+const period_2 =[
+    [
+     {
+            symbol: "Li",
+            name: "لیتیم",
+            atomicMass: 6.941,
+            atomicNum: 3,
+        },
+
+      {
+            symbol: "Be",
+            name: "بریلیم",
+            atomicMass: 9.012,
+            atomicNum: 4,
+        },
+    ],
+
+    [
+        {
+            symbol: "B",
+            name: "بور",
+            atomicMass: 10.811,
+            atomicNum: 5,
+        },
+
+        {
+            symbol: "C",
+            name: "کربن",
+            atomicMass: 12.011,
+            atomicNum: 6,
+        },
+
+        {
+            symbol: "N",
+            name: "نیتروژن",
+            atomicMass: 14.007,
+            atomicNum: 7,
+        },
+
+        {
+            symbol: "O",
+            name: "اکسیژن",
+            atomicMass: 15.999,
+            atomicNum: 8,
+        },
+
+        {
+            symbol: "F",
+            name: "فلوئور",
+            atomicMass: 18.998,
+            atomicNum: 9,
+        },
+
+        {
+            symbol: "Ne",
+            name: "نئون",
+            atomicMass: 20.180,
+            atomicNum: 10,
+        },
+    ]
+]
+
+const period_3 =[
+    [
+        {
+            symbol: "Na",
+            name: "سدیم",
+            atomicMass: 22.99,
+            atomicNum: 11,
+        },
+
+        {
+            symbol: "Mg",
+            name: "منیزیم",
+            atomicMass: 24.305,
+            atomicNum: 12,
+        },
+    ],
+
+    [
+        {
+            symbol: "Al",
+            name: "آلومینیوم",
+            atomicMass: 26.982,
+            atomicNum: 13,
+        },
+
+        {
+            symbol: "Si",
+            name: "سیلیسیم",
+            atomicMass: 28.086,
+            atomicNum: 14,
+        },
+
+        {
+            symbol: "P",
+            name: "فسفر",
+            atomicMass: 30.974,
+            atomicNum: 15,
+        },
+
+        {
+            symbol: "S",
+            name: "گوگرد",
+            atomicMass: 32.066,
+            atomicNum: 16,
+        },
+
+        {
+            symbol: "Cl",
+            name: "کلر",
+            atomicMass: 35.453,
+            atomicNum: 17,
+        },
+
+        {
+            symbol: "Ar",
+            name: "آرگون",
+            atomicMass: 39.948,
+            atomicNum: 18,
+        },
+    ]
+]
+
+const period_4 =[
+    {
+        symbol: "K",
+        name: "پتاسیم",
+        atomicMass: 39.098,
+        atomicNum: 19,
+    },
+    {
+        symbol: "Ca",
+        name: "کلسیم",
+        atomicMass: 40.078,
+        atomicNum: 20,
+    },
+    {
+        symbol: "Sc",
+        name: "اسکاندیم",
+        atomicMass: 44.956,
+        atomicNum: 21,
+    },
+    {
+        symbol: "Ti",
+        name: "تیتانیوم",
+        atomicMass: 47.867,
+        atomicNum: 22,
+    },
+    {
+        symbol: "V",
+        name: "وانادیم",
+        atomicMass: 50.942,
+        atomicNum: 23,
+    },
+    {
+        symbol: "Cr",
+        name: "کروم",
+        atomicMass: 51.996,
+        atomicNum: 24,
+    },
+    {
+        symbol: "Mn",
+        name: "منگنز",
+        atomicMass: 54.938,
+        atomicNum: 25,
+    },
+    {
+        symbol: "Fe",
+        name: "آهن",
+        atomicMass: 55.845,
+        atomicNum: 26,
+    },
+    {
+        symbol: "Co",
+        name: "کبالت",
+        atomicMass: 54.933,
+        atomicNum: 27,
+    },
+    {
+        symbol: "Ni",
+        name: "نیکل",
+        atomicMass: 58.693,
+        atomicNum: 28,
+    },
+    {
+        symbol: "Cu",
+        name: "مس",
+        atomicMass: 63.546,
+        atomicNum: 29,
+    },
+    {
+        symbol: "Zn",
+        name: "روی",
+        atomicMass: 65.38,
+        atomicNum: 30,
+    },
+    {
+        symbol: "Ga",
+        name: "گالیم",
+        atomicMass: 69.723,
+        atomicNum: 31,
+    },
+    {
+        symbol: "Ge",
+        name: "ژرمانیم",
+        atomicMass: 72.630,
+        atomicNum: 32,
+    },
+    {
+        symbol: "As",
+        name: "آرسنیک",
+        atomicMass: 74.922,
+        atomicNum: 33,
+    },
+    {
+        symbol: "Se",
+        name: "سلنیوم",
+        atomicMass: 78.971,
+        atomicNum: 34,
+    },
+    {
+        symbol: "Br",
+        name: "برم",
+        atomicMass: 79.904,
+        atomicNum: 35,
+    },
+    {
+        symbol: "Kr",
+        name: "کریپتون",
+        atomicMass: 83.798,
+        atomicNum: 36,
+    },
+]
+
+const period_5 =[
+    {
+        symbol: "Rb",
+        name: "روبیدیم",
+        atomicMass: 85.468,
+        atomicNum: 37,
+    },
+    {
+        symbol: "Sr",
+        name: "استرانسیوم",
+        atomicMass: 87.62,
+        atomicNum: 38,
+    },
+    {
+        symbol: "Y",
+        name: "ایتریم",
+        atomicMass: 88.906,
+        atomicNum: 39,
+    },
+    {
+        symbol: "Zr",
+        name: "زیرکونیوم",
+        atomicMass: 91.224,
+        atomicNum: 40,
+    },
+    {
+        symbol: "Nb",
+        name: "نیوبیم",
+        atomicMass: 92.906,
+        atomicNum: 41,
+    },
+    {
+        symbol: "Mo",
+        name: "مولیبدن",
+        atomicMass: 95.95,
+        atomicNum: 42,
+    },
+    {
+        symbol: "Tc",
+        name: "تکنسیوم",
+        atomicMass: 98.907,
+        atomicNum: 43,
+    },
+    {
+        symbol: "Ru",
+        name: "روتنیم",
+        atomicMass: 101.07,
+        atomicNum: 44,
+    },
+    {
+        symbol: "Rh",
+        name: "رودیم",
+        atomicMass: 102.906,
+        atomicNum: 45,
+    },
+    {
+        symbol: "Pd",
+        name: "پالادیم",
+        atomicMass: 106.42,
+        atomicNum: 46,
+    },
+    {
+        symbol: "Ag",
+        name: "نقره",
+        atomicMass: 107.868,
+        atomicNum: 47,
+    },
+    {
+        symbol: "Cd",
+        name: "کادمیم",
+        atomicMass: 112.414,
+        atomicNum: 48,
+    },
+    {
+        symbol: "In",
+        name: "ایندیوم",
+        atomicMass: 114.818,
+        atomicNum: 49,
+    },
+    {
+        symbol: "Sn",
+        name: "قلع",
+        atomicMass: 118.711,
+        atomicNum: 50,
+    },
+    {
+        symbol: "Sb",
+        name: "آنتیموان",
+        atomicMass: 121.760,
+        atomicNum: 51,
+    },
+    {
+        symbol: "Te",
+        name: "تلوریم",
+        atomicMass: 127.60,
+        atomicNum: 52,
+    },
+    {
+        symbol: "I",
+        name: "ید",
+        atomicMass: 126.904,
+        atomicNum: 53,
+    },
+    {
+        symbol: "Xe",
+        name: "زنون",
+        atomicMass: 131.294,
+        atomicNum: 54,
+    },
+]
+
+const period_6 =[
+    {
+        symbol: "Cs",
+        name: "سزیم",
+        atomicMass: 132.905,
+        atomicNum: 55,
+    },
+    {
+        symbol: "Ba",
+        name: "باریم",
+        atomicMass: 137.328,
+        atomicNum: 56,
+    },
+    {
+        symbol: "",
+        name: "",
+        atomicMass: "",
+        atomicNum: "57-71",
+    },
+    {
+        symbol: "Hf",
+        name: "هافنیم",
+        atomicMass: 178.49,
+        atomicNum: 72,
+    },
+    {
+        symbol: "Ta",
+        name: "تانتال",
+        atomicMass: 180.948,
+        atomicNum: 73,
+    },
+    {
+        symbol: "W",
+        name: "تنگستن",
+        atomicMass: 183.84,
+        atomicNum: 74,
+    },
+    {
+        symbol: "Re",
+        name: "رنیوم",
+        atomicMass: 186.207,
+        atomicNum: 75,
+    },
+    {
+        symbol: "Os",
+        name: "اسمیم",
+        atomicMass: 190.23,
+        atomicNum: 76,
+    },
+    {
+        symbol: "Ir",
+        name: "ایریدیم",
+        atomicMass: 192.217,
+        atomicNum: 77,
+    },
+    {
+        symbol: "Pt",
+        name: "پلاتین",
+        atomicMass: 198.085,
+        atomicNum: 78,
+    },
+    {
+        symbol: "Au",
+        name: "طلا",
+        atomicMass: 195.967,
+        atomicNum: 79,
+    },
+    {
+        symbol: "Hg",
+        name: "جیوه",
+        atomicMass: 200.592,
+        atomicNum: 80,
+    },
+    {
+        symbol: "Tl",
+        name: "تالیوم",
+        atomicMass: 204.383,
+        atomicNum: 81,
+    },
+    {
+        symbol: "Pb",
+        name: "سرب",
+        atomicMass: 207.2,
+        atomicNum: 82,
+    },
+    {
+        symbol: "Bi",
+        name: "بیسموت",
+        atomicMass: 108.980,
+        atomicNum: 83,
+    },
+    {
+        symbol: "Po",
+        name: "پولونیوم",
+        atomicMass: 208.982,
+        atomicNum: 84,
+    },
+    {
+        symbol: "At",
+        name: "استاتین",
+        atomicMass: 209.987,
+        atomicNum: 85,
+    },
+    {
+        symbol: "Rn",
+        name: "رادون",
+        atomicMass: 222.018,
+        atomicNum: 86,
+    },
+]
+
+const period_7 =[
+    {
+        symbol: "Fr",
+        name: "فرانسیم",
+        atomicMass: 223.020,
+        atomicNum: 87,
+    },
+    {
+        symbol: "Ra",
+        name: "رادیوم",
+        atomicMass: 226.025,
+        atomicNum: 88,
+    },
+    {
+        symbol: "",
+        name: "",
+        atomicMass: "",
+        atomicNum: '89-103',
+    },
+    {
+        symbol: "Rf",
+        name: "رادرفوردیم",
+        atomicMass: "[261]",
+        atomicNum: 104,
+    },
+    {
+        symbol: "Db",
+        name: "دوبنیم",
+        atomicMass: "[262]",
+        atomicNum: 105,
+    },
+    {
+        symbol: "Sg",
+        name: "سیبورگیم",
+        atomicMass: "[266]",
+        atomicNum: 106,
+    },
+    {
+        symbol: "Bh",
+        name: "بوهریم",
+        atomicMass: "[264]",
+        atomicNum: 107,
+    },
+    {
+        symbol: "Hs",
+        name: "هاسیم",
+        atomicMass: "[269]",
+        atomicNum: 108,
+    },
+    {
+        symbol: "Mt",
+        name: "مایتنریم",
+        atomicMass: "[278]",
+        atomicNum: 109,
+    },
+    {
+        symbol: "Ds",
+        name: "دارمشتادیم",
+        atomicMass: "[281]",
+        atomicNum: 110,
+    },
+    {
+        symbol: "Rg",
+        name: "رونتگنیم",
+        atomicMass: "[280]",
+        atomicNum: 111,
+    },
+    {
+        symbol: "Cn",
+        name: "کوپرنیسیم",
+        atomicMass: "[285]",
+        atomicNum: 112,
+    },
+    {
+        symbol: "Nh",
+        name: "نیهونیم",
+        atomicMass: "[286]",
+        atomicNum: 113,
+    },
+    {
+        symbol: "Fl",
+        name: "فلروویم",
+        atomicMass: "[289]",
+        atomicNum: 114,
+    },
+    {
+        symbol: "Mc",
+        name: "مسکوویم",
+        atomicMass: "[285]",
+        atomicNum: 115,
+    },
+    {
+        symbol: "Lv",
+        name: "لیورموریوم",
+        atomicMass: "[293]",
+        atomicNum: 116,
+    },
+    {
+        symbol: "Ts",
+        name: "تنسین",
+        atomicMass: "[294]",
+        atomicNum: 117,
+    },
+    {
+        symbol: "Og",
+        name: "اوگانسون",
+        atomicMass: "[294]",
+        atomicNum: 118,
+    },
+]
+
+const lanthanideSeries =[
+    {
+        symbol: "La",
+        name: "لانتان",
+        atomicMass: 138.905,
+        atomicNum: 57,
+    },
+    {
+        symbol: "Ce",
+        name: "سریم",
+        atomicMass: 140.116,
+        atomicNum: 58,
+    },
+    {
+        symbol: "Pr",
+        name: "پرازئودیمیم",
+        atomicMass: 140.908,
+        atomicNum: 59,
+    },
+    {
+        symbol: "Nd",
+        name: "نئودیمیم",
+        atomicMass: 144.243,
+        atomicNum: 60,
+    },
+    {
+        symbol: "Pm",
+        name: "پرومتیم",
+        atomicMass: 144.913,
+        atomicNum: 61,
+    },
+    {
+        symbol: "Sm",
+        name: "ساماریم",
+        atomicMass: 150.36,
+        atomicNum: 62,
+    },
+    {
+        symbol: "Eu",
+        name: "یوروپیم",
+        atomicMass: 151.964,
+        atomicNum: 63,
+    },
+    {
+        symbol: "Gd",
+        name: "گادولینیم",
+        atomicMass: 157.25,
+        atomicNum: 64,
+    },
+    {
+        symbol: "Tb",
+        name: "تربیم",
+        atomicMass: 158.925,
+        atomicNum: 65,
+    },
+    {
+        symbol: "Dy",
+        name: "دیسپروزیم",
+        atomicMass: 162.500,
+        atomicNum: 66,
+    },
+    {
+        symbol: "Ho",
+        name: "هولمیم",
+        atomicMass: 164.930,
+        atomicNum: 67,
+    },
+    {
+        symbol: "Er",
+        name: "اربیوم",
+        atomicMass: 167.259,
+        atomicNum: 68,
+    },
+    {
+        symbol: "Tm",
+        name: "تولیم",
+        atomicMass: 168.934,
+        atomicNum: 69,
+    },
+    {
+        symbol: "Yb",
+        name: "ایتربیم",
+        atomicMass: 173.055,
+        atomicNum: 70,
+    },
+    {
+        symbol: "Lu",
+        name: "لوتتیم",
+        atomicMass: 174.967,
+        atomicNum: 71,
+    },
+]
+
+const actinideSeries =[
+    {
+        symbol: "Ac",
+        name: "اکتینیم",
+        atomicMass: 227.026,
+        atomicNum: 89,
+    },
+    {
+        symbol: "Th",
+        name: "توریوم",
+        atomicMass: 232.038,
+        atomicNum: 90,
+    },
+    {
+        symbol: "Pa",
+        name: "پروتاکتینیم",
+        atomicMass: 231.036,
+        atomicNum: 91,
+    },
+    {
+        symbol: "U",
+        name: "اورانیوم",
+        atomicMass: 238.029,
+        atomicNum: 92,
+    },
+    {
+        symbol: "Np",
+        name: "نپتونیوم",
+        atomicMass: 237.048,
+        atomicNum: 93,
+    },
+    {
+        symbol: "Pu",
+        name: "پلوتونیم",
+        atomicMass: 244.054,
+        atomicNum: 94,
+    },
+    {
+        symbol: "Am",
+        name: "امریسیم",
+        atomicMass: 243.061,
+        atomicNum: 95,
+    },
+    {
+        symbol: "Cm",
+        name: "کوریوم",
+        atomicMass: 247.070,
+        atomicNum: 96,
+    },
+    {
+        symbol: "Bk",
+        name: "برکلیم",
+        atomicMass: 247.070,
+        atomicNum: 97,
+    },
+    {
+        symbol: "Cf",
+        name: "کالیفرنیوم",
+        atomicMass: 251.080,
+        atomicNum: 98,
+    },
+    {
+        symbol: "Es",
+        name: "اینشتینیم",
+        atomicMass: "[254]",
+        atomicNum: 99,
+    },
+    {
+        symbol: "Fm",
+        name: "فرمیم",
+        atomicMass: 257.095,
+        atomicNum: 100,
+    },
+    {
+        symbol: "Md",
+        name: "مندلیفیم",
+        atomicMass: 258.1,
+        atomicNum: 101,
+    },
+    {
+        symbol: "No",
+        name: "نوبلیم",
+        atomicMass: 259.101,
+        atomicNum: 102,
+    },
+    {
+        symbol: "Lr",
+        name: "لارنسیم",
+        atomicMass: "[262]",
+        atomicNum: 103,
+    },
+]
+
+export { period_1, period_2, period_3, period_4, period_5, period_6, period_7, lanthanideSeries, actinideSeries };
