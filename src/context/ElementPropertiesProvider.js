f21b9ff06e4f3eebcc2d13ffeed1033ff10e9e2b
Arash Zarei <arashzarei.js@gmail.com>
2023-04-03 15:15:46 +0430

    fixed bug.

--- src/context/ElementPropertiesProvider.js
+++ src/context/ElementPropertiesProvider.js
@@ -1,20 +1,19 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext } from "react";
 
 export const ShowPropertiesContext = createContext();
 
-const ElementPropertiesProvider = ({ childer }) => {
+const ElementPropertiesProvider = ({ children }) => {
+  const [isShow, setIsShow] = useState(false);
 
-    const [isSow, setIsShow] = useState(false);
+  const changed = () =>{
+    setIsShow(!isShow)
+  }
 
-    const changed = () => {
-        setIsShow(!isSow)
-    }
-
-    return (
-        <ShowPropertiesContext.Provider value={{ isSow, changed }}>
-            {childer}
-        </ShowPropertiesContext.Provider>
-    );
+  return (
+    <ShowPropertiesContext.Provider value={{ isShow, changed }}>
+      {children}
+    </ShowPropertiesContext.Provider>
+  );
 };
 
 export default ElementPropertiesProvider;
