a99998acd348ac40ba0a3c47f388337bd183ae3b
Arash Zarei <arashzarei.js@gmail.com>
2023-04-05 15:41:34 +0430

    change light mode to dark mode.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import ElementPropertiesProvider from "./context/ElementPropertiesProvider";
 const App = () => {
   return (
     <ElementPropertiesProvider>
-      <div className="App w-[100%] h-[100vh] pt-5 relative">
-        <h1 className="h1 text-center font-primary">جدول نتاوبی</h1>
+      <div className="App w-[100%] h-[100vh] pt-5 relative bg-black">
+        <h1 className="h1 text-white text-center font-primary">جدول نتاوبی</h1>
         <div className="container h-[80vh] mx-auto overflow-x-auto">
           <BodyTable />
         </div>
